48771dcf7cb35ae9d6346ced5dcf91460d2b5ff2
Owoeye Samuel <owoeye1321@gmail.com>
2023-09-09 13:18:07 +0100

    added model architecture

--- src/model/User.ts
+++ src/model/User.ts
@@ -4,10 +4,13 @@ import uniqueValidator from "mongoose-unique-validator";
 export interface IUser {
   name: string;
 }
-export default interface IUserModel extends Document, IUser {}
+export default interface IUserModel extends Document, IUser {
+  sendRegisterationEmail(): Promise<void>;
+}
 const user = new Schema({
   name: { type: String },
 });
 user.plugin(uniqueValidator);
+user.methods.sendRegisterationEmail = async () => {};
 
 export const User = model<IUserModel>("user", user);
